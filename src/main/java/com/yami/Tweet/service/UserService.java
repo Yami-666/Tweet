d6ad44d9662374508671163f6cc2661a9d0d84f6
Yami-666 <trishin.m.2002@gmail.com>
2020-09-07 09:23:40 +0300

    Added Bootstrap UI

--- src/main/java/com/yami/Tweet/service/UserService.java
+++ src/main/java/com/yami/Tweet/service/UserService.java
@@ -1,11 +1,18 @@
 package com.yami.Tweet.service;
 
+import com.yami.Tweet.domain.Role;
+import com.yami.Tweet.domain.User;
 import com.yami.Tweet.repos.UserRepo;
+import freemarker.template.utility.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -15,5 +22,24 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername((user.getUsername()));
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+
+        }
+
+        return true;
     }
 }
